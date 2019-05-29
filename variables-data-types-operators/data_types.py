b8c77e15adabde153d73d38024ef0541731980ac
Isabella Jones <isabellajones71@gmail.com>
2019-05-29 10:47:17 +0100

    added more notes

---
+++ variables-data-types-operators/data_types.py
@@ -0,0 +1,41 @@
+#Numeric types
+# numerical types are numbers, intergers, flotas or composites.
+# In python we'll mostly deal with int and float.#
+num1 = 20
+print(type(num1))
+num2 =20.0
+print(type(num2))
+
+#Decaring int or floats we use numerical characters without quotes.
+# Type python standard library and introduce python documentation
+# https://docs.python.org/3/library/stdtypes.html#numeric-types-int-float-complex
+res1 = 10+30
+res2 = 30-10
+res3 = 20% 2
+
+print(res1)
+print(res2)
+print(res3)
+
+# Logical operators -to equate something
+num_a = 10
+num_b = 10
+num_c = 13
+#bigger than
+print(num_b > num_c)print(num_b> num_c)
+#Smaller than
+print(num_b < num_c)
+#Bigger than or equal
+print(num_b >= num_c)
+#Smaller than or equal
+
+print(num_b <= num_c)
+#Check if the same
+print (num_a == num_b)
+##Lastly Long and Complex numbers
+# Long is an integer of unlimited size
+#Complex are numbers that have an addtional +bj
+# b is a float and j is the square root of -1(which is an imaginary number)
+
+#Strings
+
